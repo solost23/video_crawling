2ac9788097422c5bbb7456410333bf7f8175d992
ty <2805347171@qq.com>
2022-05-25 22:35:47 +0800

    搭建基本框架

---
+++ cmd/main.py
@@ -0,0 +1,12 @@
+# this is a video_crawling
+
+import os
+import sys
+# 默认从文件所在路径下找，所以要加入项目路径，才会从项目开始找
+sys.path.append(os.getcwd())
+
+from server import server
+
+if __name__ == '__main__':
+    server.run()
+
